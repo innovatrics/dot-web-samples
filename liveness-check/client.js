752403d6872815baaf38feb582dc7dd6a5be929a
Dusan Hlavaty <dusan.hlavaty@innovatrics.com>
2020-03-31 08:51:31 +0200

    Fixed liveness check breaking change between CORE server v5.1.0 and v5.2.0

--- liveness-check/client.js
+++ liveness-check/client.js
@@ -77,7 +77,7 @@ function displayError(text) {
 async function sendLivenessCheckRequest(results) {
   const requestData = await buildRequestData(results);
 
-  const response = await fetch("/api/v5/face/check-liveness?type=active", {
+  const response = await fetch("/api/v5/face/check-liveness-active", {
     method: "POST",
     body: JSON.stringify(requestData),
     headers: {
